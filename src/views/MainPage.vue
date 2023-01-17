758cc74f9d40cea8f8beda51d3b91ea1e31829c9
yoojaeyeong <liujeayat@gmail.com>
2023-01-17 17:59:25 +0900

    eslint 제거, 개발향후 분리된 브렌치로 개발 필요,vue 컴포넌트 - 옵션API 스타일로 추가

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,16 +1,56 @@
-<script setup>
-// document.title = "홈";
-import { ref, onMounted } from "vue";
+
+<script >
+import { ref } from "vue";
+
+
+
+export default {
+  data(){
+
+    const title = ref("MainPageTitle");
+    const tempList = [
+      {"key":"val1"},
+      {"key":"val2"}
+    ];
+    return {
+      tempList, title
+    }
+  },
+  beforeMount() {
+    document.title = this.title;
+
+  },
+  mounted() {
+    console.table( this.tempList);
+    console.log("age:",this.age);
+    // this.age = {
+    //   type: 3
+    // }
+  },
+  props: {
+    // 타입 체크 및 유효성 검사
+    age: {
+      type: Number,
+      default: 0,
+      required: false,
+      validator: (value) => {
+        return value >= 0
+      }
+    }
+  },
+
+}
+
 </script>
 <template>
   <div id="homevdom">
     <div class="flex flex-col border-separate">
-      <ul>
+      <ul v-for="item in tempList" >
         <!--        <li v-for="item in list"></li>-->
-        <li>sddsf</li>
+        <li class="p-3 border border-slate-300">{{item.key}}</li>
       </ul>
       <div class="p-3 border border-slate-300">
-        title 1
+        {{ age }}
       </div>
       <div class="p-3 border border-slate-300">
         title 1
